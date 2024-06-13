3ebb9cbc3c7547cb973671b0f2566b393f8e72c6
Lucas Becker <beckerlucas19@gmail.com>
2024-06-12 22:46:17 -0300

    feat: build components on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+'use client';
+
+import AppPresentation from "./components/AppPresentation/AppPresentation.client";
+import Header from "./components/Header/Header.client";
+import { LandingPageWrapper } from "./page.styles";
 
 export default function Home() {
   return (
-    <>Hello World</>
+    <LandingPageWrapper>
+      <Header />
+      <AppPresentation />
+    </LandingPageWrapper>
   );
 }
